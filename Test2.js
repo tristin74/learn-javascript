4452c5ff4796ff09d10e5cbb75745eb119928cc9
New Git Repository… <New Git Repository>
2023-08-19 16:44:26 +0700

    Giai thua

---
+++ Test2.js
@@ -0,0 +1,11 @@
+//TODO: cho mot so duong, tinh giai thua
+
+function test(x) {
+    let a = 1;
+    for (let i = 1; i <= x; i++) {
+        a = a * i;
+
+    }
+    return a;
+}
+console.log(test(3));
